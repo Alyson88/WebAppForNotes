e4700eff0ddbfcbbe050577200e334e299a2b531
Alyson Wood <alysonmw11@gmail.com>
2018-08-10 10:22:43 -0400

    initial commit

---
+++ WebAppForNotes/Controllers/CheatSheetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppForNotes.Controllers
+{
+    public class CheatSheetController : Controller
+    {
+        // GET: CheatSheet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppForNotes/Controllers/HowToController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppForNotes.Controllers
+{
+    public class HowToController : Controller
+    {
+        // GET: HowTo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
